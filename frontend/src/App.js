08f182a894d4c16d5d7bfd3c1b14e680aec72c7a
YSidorko <yury.sidorko@gmail.com>
2020-11-23 15:48:48 +0300

    test PR_2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Foxy Roxy! TEST PR ))
+          Edit <code>src/App.js</code> and save to reload. Foxy Roxy! TEST PR_2 ))
         </p>
         <a
           className="App-link"
