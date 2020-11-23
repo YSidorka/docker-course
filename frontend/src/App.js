b6626c1b846d4006c34d11cf9fb1375bb35c6aba
YSidorko <yury.sidorko@gmail.com>
2020-11-23 15:51:52 +0300

    TEST Develop (travis)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Foxy Roxy! TEST PR ))
+          Edit <code>src/App.js</code> and save to reload. Foxy Roxy!
+          DEVELOP CHANGE
         </p>
         <a
           className="App-link"
